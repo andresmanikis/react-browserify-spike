727e55a3ef51c13c62b7e55d32d79c539fab85e0
Andrés Manikis <andresmanikis@gmail.com>
2016-01-24 21:20:34 -0300

    Replaces grunt-contrib-watch with browserify bundled watch for highly improved performance

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,27 +2,35 @@ module.exports = function(grunt) {
   'use strict';
 
   grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
-    watch: {
-      files: [ 'src/**/*.js' ],
-      tasks: [ 'build' ]
-    },
     browserify: {
-      dist: {
+      build: {
         files: {
           "dist/bundle.js": "src/**/*.js",
         },
         options: {
           transform: [
-            [ 'babelify', { presets: 'react' } ]
+            ['babelify', { presets: 'react' }]
+          ]
+        }
+      },
+      watch: {
+        files: {
+          "dist/bundle.js": "src/**/*.js",
+        },
+        options: {
+          watch: true,
+          keepAlive: true,
+          transform: [
+            ['babelify', { presets: 'react' }]
           ]
         }
       }
     }
   });
 
-  grunt.registerTask('build', [ 'browserify' ]);
+  grunt.registerTask('build', ['browserify:build']);
+  grunt.registerTask('watch', ['browserify:watch']);
 
 };
